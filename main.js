0d3aeeff03e48eba0413671b80bee75cff09b2df
Jean Caillé <jcaille@gmail.com>
2013-05-27 23:39:28 +0200

    Updated projects

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
 	$(".skill").popover({html : true});
+	var userLang = (navigator.language) ? navigator.language : navigator.userLanguage; 
+	if(userLang == 'fr' || userLang == 'FR'){
+		$("#classic-CV-download").attr("href", "CV/CV_Jean_Caille_FR.pdf");
+	}
 })
